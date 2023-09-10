7509ab4821052914b2078f1d6e81dbd75b56b4d1
hstcris <143611263+hstcris@users.noreply.github.com>
2023-09-10 16:08:43 -0300

    Add files via upload

---
+++ Q41.py
@@ -0,0 +1,17 @@
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+numero4 = float(input("Digite o quarto número: "))
+
+
+peso1 = 1
+peso2 = 2
+peso3 = 3
+peso4 = 4
+
+soma_pesos = peso1 + peso2 + peso3 + peso4
+media_ponderada = (numero1 * peso1 + numero2 * peso2 + numero3 * peso3 + numero4 * peso4) / soma_pesos
+
+
+print("A média ponderada dos números é:", media_ponderada)

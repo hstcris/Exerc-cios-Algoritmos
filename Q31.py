cd842ba5ebbc043e234d1eb6b12fa7c4e36f455a
hstcris <143611263+hstcris@users.noreply.github.com>
2023-09-09 22:47:54 -0300

    Add files via upload

---
+++ Q31.py
@@ -0,0 +1,7 @@
+var1 = 9
+var2 = 8
+var3 = 7 
+
+media = (9 + 8 + 7)/3
+
+print(media)
---
+++ Q33.py
@@ -0,0 +1,4 @@
+n = int(input("Digite um número:"))
+n2 = int(input("Digite outro número:"))
+print (n, n2)
+        

2837e1464ad5d2d143650468f6fa71ce0e373c39
hstcris <143611263+hstcris@users.noreply.github.com>
2023-09-09 22:33:27 -0300

    Add files via upload

---
+++ Q40.py
@@ -0,0 +1,6 @@
+dividendo = int( input("Entre com o dividendo:\n"))
+divisor = int( input("Entre com o divisor:\n"))
+print("Dividendo:",dividendo)
+print("Divisor:",divisor)
+print("Quociente:",dividendo//divisor)
+print("Resto:",dividendo % divisor)

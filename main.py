ee1de28d2d0297122e7af0850378ee4b8598b06f
hstcris <143611263+hstcris@users.noreply.github.com>
2023-09-09 22:32:48 -0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-dividendo = int( input("Entre com o dividendo:\n"))
-divisor = int( input("Entre com o divisor:\n"))
-print("Dividendo:",dividendo)
-print("Divisor:",divisor)
-print("Quociente:",dividendo//divisor)
-print("Resto:",dividendo % divisor)
